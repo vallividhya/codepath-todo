5008821b85766ac5c03d36710cfb4fa803c83599
Vidhya <vidhya@ava-mac.local>
2017-08-09 17:31:39 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/simpletodo/EditItemActivity.java
@@ -0,0 +1,40 @@
+package com.codepath.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static com.codepath.simpletodo.MainActivity.ITEM_NAME;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    private final int RESPONSE_CODE = 200;
+    public static final String EDIT_ITEM_NAME = "com.codepath.simpletodo.EDIT_ITEM_NAME";
+    public static final String POS = "com.codepath.simpletodo.POSITION";
+    private int position = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+
+        Intent intent = getIntent();
+        String itemName = intent.getStringExtra(ITEM_NAME);
+        position = intent.getIntExtra(MainActivity.ITEM_POSITION, 0);
+        EditText editText = (EditText) findViewById(R.id.etUpdateItem);
+        editText.setText(itemName);
+        editText.setSelection(editText.getText().length());
+    }
+
+    public void onSaveEditItem(View view) {
+        EditText etNewItem = (EditText) findViewById(R.id.etUpdateItem);
+        String text = etNewItem.getText().toString();
+        Intent in = new Intent();
+        in.putExtra(EDIT_ITEM_NAME, text);
+        in.putExtra(POS, position);
+        setResult(RESPONSE_CODE, in);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -0,0 +1,110 @@
+package com.codepath.simpletodo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.codepath.simpletodo.EditItemActivity.EDIT_ITEM_NAME;
+import static com.codepath.simpletodo.EditItemActivity.POS;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String ITEM_NAME = "com.codepath.simpletodo.ITEM_NAME";
+    public static final String ITEM_POSITION = "com.codepath.simpletodo.ITEM_POSITION";
+    private final int REQUEST_CODE = 20;
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String text = etNewItem.getText().toString();
+        itemsAdapter.add(text);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void setupListViewListener() {
+        // Listener to delete item on long Click
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+
+        // Listener to edit item on click
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+                String itemName = (String) adapterView.getItemAtPosition(pos);
+                intent.putExtra(ITEM_POSITION, pos);
+                intent.putExtra(ITEM_NAME, itemName);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            String updatedItemName = data.getStringExtra(EDIT_ITEM_NAME);
+            int position = data.getExtras().getInt(POS);
+            items.set(position, updatedItemName);
+            itemsAdapter.notifyDataSetChanged();
+            writeItems();
+        }
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+            e.printStackTrace();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
